70477ba667125c4c752f66629c2e499ef9f6aa90
masa mekis <masa.mekis@student.um.si>
2023-04-16 22:57:51 +0200

    stirinajst

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,6 @@
 // const { _electron: electron } = require('playwright');
 const { test, expect } = require('@playwright/test');
-
+const path = require('path');
 
 // test('delete button test', async () => {
 //   const electronApp = await electron.launch({ args: ['./public/electron.js'] });
@@ -19,17 +19,48 @@ const { test, expect } = require('@playwright/test');
 //   await electronApp.close();
 // });
 
-const { chromium } = require('playwright');
+// const { chromium } = require('playwright');
 
-test('page title', (async () => {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
+// test('page title', (async () => {
+//   const browser = await chromium.launch();
+//   const context = await browser.newContext();
+//   const page = await context.newPage();
 
-  await page.goto('http://localhost:3000');
-  const title = await page.title();
-  expect(title).toBe('Cards App');
+//   await page.goto('http://localhost:3000');
+//   const title = await page.title();
+//   expect(title).toBe('Cards App');
 
-  await browser.close();
-})
-);
+//   await browser.close();
+// })
+// );
+
+const { electron } = require('playwright');
+
+describe('My Electron App', () => {
+  let app, window;
+
+  beforeAll(async () => {
+    app = await electron.launch({
+      args: ['./public/electron.js']
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  beforeEach(async () => {
+    window = await app.newWindow();
+  });
+
+  afterEach(async () => {
+    await window.close();
+  });
+
+  test('should navigate to the decks page and click the button', async () => {
+    await window.goto(`file://${path.join(__dirname, '../build/index.html')}`);
+
+
+    expect(await window.title()).toBe('Cards App');
+  });
+});
