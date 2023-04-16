e21017d24f2c6729453c80b9612e5970b3c4fb1f
masa mekis <masa.mekis@student.um.si>
2023-04-16 22:43:55 +0200

    dvanajst

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,21 +1,34 @@
-const { _electron: electron } = require('playwright');
-const { test, expect } = require('@playwright/test');
+// const { _electron: electron } = require('playwright');
+// const { test, expect } = require('@playwright/test');
 
 
-test('delete button test', async () => {
-  const electronApp = await electron.launch({ args: ['./public/electron.js'] });
-  const window = await electronApp.firstWindow();
+// test('delete button test', async () => {
+//   const electronApp = await electron.launch({ args: ['./public/electron.js'] });
+//   const window = await electronApp.firstWindow();
 
-  // Navigate to the Decks page
-  await window.goto('http://localhost:3000');
+//   // Navigate to the Decks page
+//   await window.goto('http://localhost:3000');
 
-  // Find the delete button
-  const deleteButton = await window.$('button:has-text("Delete")');
+//   // Find the delete button
+//   const deleteButton = await window.$('button:has-text("Delete")');
 
-  // Check that the delete button exists on the page
-  expect(deleteButton).not.toBeNull();
+//   // Check that the delete button exists on the page
+//   expect(deleteButton).not.toBeNull();
 
-  // Close the app
-  await electronApp.close();
-});
+//   // Close the app
+//   await electronApp.close();
+// });
 
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto('http://localhost:3000');
+  const title = await page.title();
+  console.log(title);
+
+  await browser.close();
+})();
