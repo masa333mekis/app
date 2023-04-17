82264c2e78c9489a1821d7df6153a8bdb0ef297c
masa mekis <masa.mekis@student.um.si>
2023-04-17 11:13:52 +0200

    hehe

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,6 @@
 // const { _electron: electron } = require('playwright');
-const { test, expect } = require('@playwright/test');
-const path = require('path');
+//const { test, expect } = require('@playwright/test');
+//const path = require('path');
 
 // test('delete button test', async () => {
 //   const electronApp = await electron.launch({ args: ['./public/electron.js'] });
@@ -19,26 +19,55 @@ const path = require('path');
 //   await electronApp.close();
 // });
 
-const { electron } = require('playwright-electron');
+// const { electron } = require('playwright-electron');
 
-test.describe('My Electron App', () => {
-  let app, window;
+// test.describe('My Electron App', () => {
+//   let app, window;
 
-  test.beforeAll(async () => {
-    app = await electron.launch();
-    window = await app.newWindow();
+//   test.beforeAll(async () => {
+//     app = await electron.launch();
+//     window = await app.newWindow();
+//   });
+
+//   test.afterAll(async () => {
+//     await app.close();
+//   });
+
+//   test('should navigate to the decks page and click the button', async () => {
+//     await window.goto(`file://${path.join(__dirname, '../build/index.html')}`);
+
+//     expect(await window.title()).toBe('Cards App');
+//   });
+// });
+
+const { _electron: electron } = require('playwright');
+
+(async () => {
+  // Launch Electron app.
+  const electronApp = await electron.launch({ args: ['./public/electron.js'] });
+
+  // Evaluation expression in the Electron context.
+  const appPath = await electronApp.evaluate(async ({ app }) => {
+    // This runs in the main Electron process, parameter here is always
+    // the result of the require('electron') in the main app script.
+    return app.getAppPath();
   });
+  console.log(appPath);
 
-  test.afterAll(async () => {
-    await app.close();
-  });
+  // Get the first window that the app opens, wait if necessary.
+  const window = await electronApp.firstWindow();
+  // Print the title.
+  console.log(await window.title());
+  // Capture a screenshot.
+  await window.screenshot({ path: 'intro.png' });
+  // Direct Electron console to Node terminal.
+  window.on('console', console.log);
+  // Click button.
+  await window.click('text=Click me');
+  // Exit app.
+  await electronApp.close();
+})();
 
-  test('should navigate to the decks page and click the button', async () => {
-    await window.goto(`file://${path.join(__dirname, '../build/index.html')}`);
-
-    expect(await window.title()).toBe('Cards App');
-  });
-});
 
 // const { chromium } = require('playwright');
 
